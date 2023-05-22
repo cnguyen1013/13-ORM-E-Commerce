92893cba224ad9378a39bb81ff5eb0021812d2fa
cnguyen1013 <CNguyen7@mdanderson.org>
2023-05-21 21:30:55 -0500

    fixed destroy typo

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -126,7 +126,7 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
   try {
-    const productData = Product.destory({
+    const productData = Product.destroy({
       where: {
         id: req.params.id,
       },
